8e80025290254a4fa1229457aeaef4d9dba27815
armeono <arman.hadzigrahic@zendev.se>
2022-11-01 10:11:06 +0100

    added keyboard navigation on projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,9 @@
 import { FunctionComponent, useState } from "react";
 import styles from "../styles/Projects.module.css";
 import Project from "./Project";
-import ChatImage from '../public/chat.png'
-import LibraryImage from '../public/lib.png'
-import Shopimage from '../public/shop-photo.png'
+import ChatImage from "../public/chat.png";
+import LibraryImage from "../public/lib.png";
+import Shopimage from "../public/shop-photo.png";
 
 interface ProjectsProps {}
 
@@ -14,33 +14,46 @@ const Projects: FunctionComponent<ProjectsProps> = () => {
     {
       id: 1,
       name: "Chat app",
-      description: "Real-time chat application build using Angular, Node.js and Socket.io",
-      image: ChatImage
+      description:
+        "Real-time chat application build using Angular, Node.js and Socket.io",
+      image: ChatImage,
     },
     {
       id: 2,
       name: "E-Commerce app",
-      description: "E-Commerce application built using React (Typescript) and Node.js (Prisma, Express, Typescript)",
-      image: Shopimage
+      description:
+        "E-Commerce application built using React (Typescript) and Node.js (Prisma, Express, Typescript)",
+      image: Shopimage,
     },
     {
       id: 3,
       name: "Library app",
-      description: "Library application built using Angular, Node.js and the Google Books API",
-      image: LibraryImage
+      description:
+        "Library application built using Angular, Node.js and the Google Books API",
+      image: LibraryImage,
     },
   ];
-
 
   const mod = (n: number, m: number) => {
     let result = n % m;
 
     return result >= 0 ? result : result + m;
   };
+
+  const keyPressed = (e: any) => {
+    if(e.key === "ArrowLeft") {
+      setIndex(prev => prev !== 0 ? prev - 1 : prev = 2);
+    }else if (e.key === "ArrowRight"){
+      setIndex(prev => prev !== 2 ? prev + 1 : prev = 0);
+    }
+  };
+
   return (
     <div
       id="projects"
       className="w-screen h-screen flex justify-center items-center"
+      onKeyDown={(e) => keyPressed(e)}
+      tabIndex={-1}
     >
       <div className={styles.carousel}>
         {projects.map((item: any, i: any) => {
