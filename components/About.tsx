8e20a731668e33a9b4b4631b4019a8268b0afe9a
armeono <arman.hadzigrahic@zendev.se>
2022-11-02 16:33:51 +0100

    worked on about page

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { FunctionComponent, Ref } from "react";
 import portfolioPic from "../public/portfoliopic.png";
+import styles from "../styles/About.module.css";
+import polygon from "../public/polygon.svg";
 
 interface AboutProps {}
 
@@ -8,18 +10,38 @@ const About: FunctionComponent<AboutProps> = () => {
   return (
     <div
       id="about"
-      className="observe w-screen h-screen scroll-smooth flex justify-center items-center"
+      className="w-screen h-screen scroll-smooth flex flex-col justify-center items-center lg:flex-row"
     >
-      <div className="relative h-96 w-96 rounded-full overflow-hidden">
-
-        <div className="absolute h-full w-full bg-gradient-to-r from-cyan-400 via-cyan-900 to-transparent rotate-180"></div>
-        <Image
-          src={portfolioPic}
-          alt="portfolio-pic"
-          className="absolute scale-150 right-5"
-        />
+      <div className="relative w-1/2 h-2/3 flex justify-center items-center">
+        <div className="relative w-[500px] h-full flex justify-center items-center">
+          <div className={styles.shape}></div>
+          <div className={styles.shape2}></div>
+          <Image
+            src={polygon}
+            alt="polygon"
+            className="absolute bottom-[75px] right-[-75px] "
+          />
+          <div className="relative h-96 w-96 overflow-hidden">
+            <Image
+              src={portfolioPic}
+              alt="portfolio-pic"
+              className="absolute scale-150 right-5 z-10"
+            />
+          </div>
+        </div>
       </div>
-      <div className="w-1/2"></div>
+
+      <div className="w-1/2 h-fit">
+        <div className="w-4/5 relative flex flex-col justify-center items-center z-10 gap-4">
+          <h2 className="text-7xl">About me</h2>
+          <p className="text-3xl text-center">
+            I'm a 21 year old <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">Computer Science and Engineering</span> student from
+            Sarajevo. I have a passion for creating things and an interest in 
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400"> Full-Stack Development</span> and <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">AI</span>. 
+          </p>
+          <p className="text-2xl">Besides programming, I enjoy playing guitar, chess and doing martial arts.</p>
+        </div>
+      </div>
     </div>
   );
 };
