2960de1a03eb232e2290ceee4ee9beb9772f1196
armeono <arman.hadzigrahic@zendev.se>
2022-11-03 02:21:21 +0100

    fixed responsivness issue with about photo and added animation

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FunctionComponent, Ref } from "react";
+import { FunctionComponent, Ref, useEffect, useRef } from "react";
 import portfolioPic from "../public/portfoliopic.png";
 import styles from "../styles/About.module.css";
 import polygon from "../public/polygon.svg";
@@ -7,26 +7,52 @@ import polygon from "../public/polygon.svg";
 interface AboutProps {}
 
 const About: FunctionComponent<AboutProps> = () => {
+  const observer = useRef<any>(null);
+  const hiddenElements = useRef<any>(null);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log("istrue");
+          entry.target.classList.add(styles.show);
+        } else {
+          entry.target.classList.remove(styles.show);
+        }
+      });
+    });
+
+    hiddenElements.current = document.querySelectorAll(".hide");
+    hiddenElements.current.forEach((el: any) => observer.current.observe(el));
+  }, []);
+
   return (
     <div
       id="about"
       className="w-screen h-screen scroll-smooth flex flex-col justify-center items-center lg:flex-row"
     >
-      <div className="relative w-1/2 h-2/3 flex justify-center items-center">
-        <div className="relative w-[500px] h-full flex justify-center items-center">
-          <div className={styles.shape}></div>
-          <div className={styles.shape2}></div>
+      <div className="relative w-1/2 h-[650px] flex justify-center items-center">
+        <div className="relative w-[500px] h-full flex justify-center items-center 2xl:scale-150">
+          <div
+            className={`${styles.hide} hide absolute h-[300px] w-[300px] bg-cyan-400 top-[213px] right-0 z-8`}
+            style={{ clipPath: "polygon(25% 0%, 100% 0, 75% 100%, 0% 100%" }}
+          ></div>
+          <div
+            className={` ${styles.hide} hide absolute h-[300px] w-[300px] bg-gradient-to-t from-fuchsia-400 to-transparent top-[240px] right-[-30px] z-[-1]`}
+            style={{ clipPath: "polygon(25% 0%, 100% 0, 75% 100%, 0% 100%" }}
+          ></div>
           <Image
             src={polygon}
             alt="polygon"
-            className="absolute bottom-[75px] right-[-75px] "
+            className={`${styles.hide} hide h-[300px] w-[300px] absolute top-[275px] right-[-75px] z-[-1]`}
           />
-          <div className="relative h-96 w-96 overflow-hidden">
-            <Image
+          <div className={`${styles.imageContainer} ${styles.hide} hide`}>
+             <Image
               src={portfolioPic}
               alt="portfolio-pic"
-              className="absolute scale-150 right-5 z-10"
+              className={`hide ${styles.image}`}
             />
+      
           </div>
         </div>
       </div>
@@ -35,11 +61,26 @@ const About: FunctionComponent<AboutProps> = () => {
         <div className="w-4/5 relative flex flex-col justify-center items-center z-10 gap-4">
           <h2 className="text-7xl">About me</h2>
           <p className="text-3xl text-center">
-            I&apos;m a 21 year old <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">Computer Science and Engineering</span> student from
-            Sarajevo. I have a passion for creating things and an interest in 
-            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400"> Full-Stack Development</span> and <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">AI</span>. 
+            I&apos;m a 21 year old{" "}
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">
+              Computer Science and Engineering
+            </span>{" "}
+            student from Sarajevo. I have a passion for creating things and an
+            interest in
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">
+              {" "}
+              Full-Stack Development
+            </span>{" "}
+            and{" "}
+            <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-500 to-cyan-400">
+              AI
+            </span>
+            .
           </p>
-          <p className="text-2xl">Besides programming, I enjoy playing guitar, chess and doing martial arts.</p>
+          <p className="text-2xl text-center">
+            Besides programming, I enjoy playing guitar, chess and doing martial
+            arts.
+          </p>
         </div>
       </div>
     </div>
